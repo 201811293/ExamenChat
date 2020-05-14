a024a004cc88ff2708408eda5967cff5a3447f67
201811293 <maycol.cahuana@upeu.edu.pe>
2020-05-13 23:03:57 -0500

    guardar archivos

--- src/chatjavainge/Cliente.java
+++ src/chatjavainge/Cliente.java
@@ -8,6 +8,7 @@ package chatjavainge;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -20,7 +21,7 @@ public static void main(String argv[]) {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));//para que lea
             Socket socket;
     byte[] mensaje_bytes = new byte[256];
-        String mensaje="";
+        String mensaje="",mensajeCliente="";  
     try {
         socket = new Socket("127.0.0.1",6000);
         DataOutputStream out =
--- src/chatjavainge/Servidor.java
+++ src/chatjavainge/Servidor.java
@@ -8,7 +8,9 @@ package chatjavainge;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;//para el txt
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,28 +22,60 @@ public class Servidor {
     
 public static void main(String argv[]) {
     ServerSocket socket;
-    BufferedReader in2 = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader in2 = new BufferedReader(new InputStreamReader(System.in));//entrada
     
-    byte[] mensaje_bytes = new byte[256];
-        String mensaje="";
-boolean fin = false;
+        byte[] mensaje_bytes = new byte[256];
+        String mensaje="",mensajeCliente="";  
+        String Final = null;
     try { 
         socket = new ServerSocket(6000);
+        
         Socket socket_cli = socket.accept();
         DataInputStream in =
     new DataInputStream(socket_cli.getInputStream());
        
-    DataOutputStream out =new DataOutputStream(socket_cli.getOutputStream());    
+    DataOutputStream out =new DataOutputStream(socket_cli.getOutputStream());    //salida
         System.out.println("Envie mensaje: ");
-    do {
-        String mensajeCliente ="";
-        mensajeCliente = in.readUTF();
-        System.out.println("Recibiendo mensaje: "+mensajeCliente);
         
-        mensaje = in2.readLine();
-        out.writeUTF(mensaje);
-        out.flush();
-    } while (!mensaje.startsWith("fin"));
+        
+        while(!mensaje.equals("fin")){  //palabra para cerra
+                mensaje=in.readUTF();  
+                System.out.println("Cliente: "+mensaje);  
+                mensajeCliente=in2.readLine();  
+                out.writeUTF(mensajeCliente);  
+                out.flush();  
+                
+                
+                
+                if(Final == null){
+                    Final= "Cliente: "+mensaje+"\n"+"Servidor: "+mensajeCliente+"\n";
+                }else{
+                    Final= Final+"Cliente: "+mensaje+"\n"+"Servidor: "+mensajeCliente+"\n";
+                  
+                }
+        
+        }
+        in.close();  
+        socket.close();  
+        int i = 1;
+        String ruta = "C:/Users/Usuario/Documents/ExamenIngEder/"+ i +".txt";
+        File archivo = new File(ruta); 
+        
+        
+        
+        
+        do{
+            ++i;
+            ruta = "C:/Users/Usuario/Documents/ExamenIngEder/"+ i +".txt";
+            archivo = new File(ruta);
+        }while (archivo.exists());//mientras
+
+        
+        PrintWriter writer = new PrintWriter("C:/Users/Usuario/Documents/ExamenIngEder/"+ i +".txt", "UTF-8");
+        writer.println(Final);
+        writer.close();
+        
+     
         }
     catch (Exception e) {
         System.err.println(e.getMessage());
